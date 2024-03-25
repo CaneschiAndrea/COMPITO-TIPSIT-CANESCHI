77f7ede67a09d0cd676edcdcffaa117a96028895
lai2-24 <lai2-24@itismeucci.com>
2024-03-25 11:41:13 +0100

    secondo commit

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 use Slim\Factory\AppFactory;
+use Slim\Psr7\Response;
+use Slim\Psr7\Request;
 
 require __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/controllers/SquadraController.php';
@@ -8,8 +10,6 @@ require_once __DIR__ . '/controllers/TorneoController.php';
 require_once __DIR__ . '/controllers/PartitaController.php';
 
 $app = AppFactory::create();
-
-
 
 $app->get('/torneo/{codice}', 'TorneoController:getTorneo'); 
 $app->get('/squadre', 'SquadraController:getSquadre'); 
@@ -19,7 +19,10 @@ $app->get('/torneo/{codice}/partite/{id_partita}', 'PartitaController:getPartite
 $app->get('/torneo/{codice}/concluso', 'TorneoController:partiteGiocate');
 $app->get('/torneo/{codice}/classifica', 'TorneoController:getClassifica');
 
-
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Benvenuto su questa applicazione!");
+    return $response;
+});
 
 
 
